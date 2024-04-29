d7965878e03b9e47597f06c2ce5d73b2dc310ca9
helene <helene.maja@hotmail.dk>
2024-04-29 22:23:49 +0200

    adjust prj14

--- project_14.c
+++ project_14.c
@@ -2,36 +2,41 @@
 #include <stdio.h>
 
 // todo
-// 1. read input and store in char[n]
-// 2. stop at '.' and store in seperate char variable
+// 1. read input and store in char[n] - løst
+// 2. stop at '.' and store in seperate char variable - løst
 // 3. start from the input from 1. and print the words from the back
 // 4. print '.'
 
 
 int main()
 {
-    char user_input[11];
-    int num = sizeof(user_input) / sizeof(user_input[0]);
     char ch;
-    int len = 0;
+    int len = 50;
+    int len_var = 0;
+    char user_input[len];
 
     printf("Enter a sentence: \n");
-    while ((ch = getchar()) != '\n')
+    while ((ch = getchar()) != '\n' && ch != '!' && ch != '.' && ch != '?')
     {
-        len++;
+        user_input[len_var] = ch;
+        len_var++;
     }
-    
+    // printf("%c", ch);
 
-    
-    
-    
-    
-
-    // for (int i = 0; i < 11; i++)
+    // for (int i = 0; i < len_var; i++)
     // {
     //     printf("%c", user_input[i]);
+
     // }
     // printf("\n");
+
+    for (int i = len_var; i > -1; i--)
+    {
+
+        printf("%c", user_input[i]);
     
+    }
+    printf("\n");
+
 
 }
