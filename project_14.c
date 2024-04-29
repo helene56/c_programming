692aa94949f5be228676ea19ac8b4d68a50a530f
helene <helene.maja@hotmail.dk>
2024-04-29 10:59:09 +0200

    basic structure add and todo add to project14

--- project_14.c
+++ project_14.c
@@ -1,3 +1,15 @@
 // see page 180 in book
 #include <stdio.h>
 
+// todo
+// 1. read input and store in char[n]
+// 2. stop at '.' and store in seperate char variable
+// 3. start from the input from 1. and print the words from the back
+// 4. print '.'
+
+
+int main()
+{
+    char user_input[50];
+
+}
