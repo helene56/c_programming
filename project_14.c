85791b164d693aae68fdec670af095cc51c39e3c
helene <helene.maja@hotmail.dk>
2024-04-30 14:07:20 +0200

    finished the task prj14, now ready for merge with main branch

--- project_14.c
+++ project_14.c
@@ -4,9 +4,8 @@
 // todo
 // 1. read input and store in char[n] - løst
 // 2. stop at '.' and store in seperate char variable - løst
-// 3. start from the input from the last word from 1. and print then search for the next last word
+// 3. start from the input from the last word from 1. and print then search for the next last word - løst
 // 4. print '.' - løst
-
 
 int main()
 {
@@ -14,21 +13,38 @@ int main()
     int len = 50;
     int len_var = 0;
     char user_input[len];
-
-    printf("Enter a sentence: \n");
+    
+    printf("Enter a sentence: ");
     while ((ch = getchar()) != '\n' && ch != '!' && ch != '.' && ch != '?')
     {
         user_input[len_var] = ch;
         len_var++;
     }
-    
+    printf("Reversal of sentence: ");
+    int space_num = len_var;
+
     for (int i = len_var; i > -1; i--)
     {
-
-        printf("%c", user_input[i]);
+        if (user_input[i] == ' ')
+        {
+            int n = i+1;
+            for (n; n < space_num; n++)
+            {
+                printf("%c", user_input[n]);    
+            }
+            space_num = i;
+            printf(" ");
+        }
     
     }
+
+    if (space_num != 0)
+    {
+        for (int j = 0; j < space_num; j++)
+        {
+            printf("%c", user_input[j]);
+        }
+    }
+    printf("%c", ch);
     printf("\n");
-
-
 }
