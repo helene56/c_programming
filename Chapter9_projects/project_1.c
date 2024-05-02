1f9ee77d76d12efe9c2083f55db4e36bb925069c
helene <helene.maja@hotmail.dk>
2024-05-02 22:45:31 +0200

    add function to prj1

--- Chapter9_projects/project_1.c
+++ Chapter9_projects/project_1.c
@@ -5,7 +5,11 @@
 // 2.1 selection_sort must do: search the array to find the largest element, then move it to the last position in the array
 // 2.2 call it self and sort the first element n-1 of the array 
 #include <stdio.h>
+
 #define SIZE 50
+
+void selection_sort(int array[], int lenght);
+
 
 int main()
 {
@@ -16,10 +20,54 @@ int main()
     printf("Enter a series of integers: ");
     while ((input = getchar()) != '\n')
     {
-        user_array[count] = input;
-        count++;
+        // user_array[count] = input;
+        // count++;
     }
 
+    printf("count: %d\n", count);
 
+    printf("numbers in array: ");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d", user_array[i]);
+    }
+    printf("\n");
+
+    selection_sort(user_array, count);
+    printf("numbers sorted test: ");
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d", user_array[i]);
+    }
+    printf("\n");
+
+}
+
+void selection_sort(int array[], int lenght)
+{
+    // finds the largest number in the array
+    int largest_num = 0;
+    int position = 0;
+
+    for (int i = 0; i < lenght; i++)
+    {
+        if (array[i] > largest_num)
+        {
+            largest_num = array[i];
+            position = i;
+        }
+    }
+
+    // puts it in the last position of the array
+    array[position] = 0;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (i > position)
+        {
+            array[i] = array[i-1];
+        }
+    }
+
+    array[lenght-1] = largest_num;
 
 }
