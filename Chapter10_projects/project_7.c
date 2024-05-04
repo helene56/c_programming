eefb0ba80889f9ffc960199b0114a0139eaeedd6
helene <helene.maja@hotmail.dk>
2024-05-04 17:23:33 +0200

    update process_digit function chapter10 prj7

--- Chapter10_projects/project_7.c
+++ Chapter10_projects/project_7.c
@@ -17,12 +17,13 @@ int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0},
                        {1, 1, 1, 1, 1, 1, 1},
                        {1, 1, 1, 1, 0, 1, 1}};
 
-int digit_pos[6][2] = {{0, 1},
+int digit_pos[7][2] = {{0, 1},
                        {1, 2},
                        {2, 2},
-                       {3, 1},
+                       {2, 1},
                        {2, 0},
-                       {1, 0}};
+                       {1, 0},
+                       {1, 1}};
 
 char ch = ' ';
 
@@ -32,7 +33,16 @@ void print_digits_array(void);
 
 int main()
 {
+    char user_input;
+    clear_digits_array();
     printf("Enter a number: ");
+    while ((user_input = getchar()) != '\n')
+    {
+        process_digit(user_input - '0', 0);
+        // putchar(user_input);
+    }
+    print_digits_array();
+
 }
 
 void clear_digits_array(void)
@@ -49,27 +59,34 @@ void clear_digits_array(void)
 void process_digit(int digit, int position)
 {
     for (int i = 0; i < 7; i++)
-    if (segments[digit][i])
     {
-        if (i == 0 || i == 3 || i == 6)
+        if (segments[digit][i])
         {
-            ch = '_';
-        }
-        else
-        {
-            ch = '|';
-        }
-        int first_pos = digit_pos[i][0];
-        int second_pos = digit_pos[i][1];
+            if (i == 0 || i == 3 || i == 6)
+            {
+                ch = '_';
+            }
+            else
+            {
+                ch = '|';
+            }
+            int first_pos = digit_pos[i][0];
+            int second_pos = digit_pos[i][1];
+            // printf("i: %d\n", i);
+            // printf("first pos: %d\n", first_pos);
+            // printf("second pos: %d\n", second_pos);
 
-        digits[first_pos][second_pos] = ch;
+            digits[first_pos][second_pos] = ch;
+        }
     }
+    
 }
 
 void print_digits_array(void)
 {
     for (int i = 0; i < 4; i++)
     {
+        
         for (int j = 0; j < MAX_DIGITS * 4; j++)
         {
             printf("%c", digits[i][j]);
