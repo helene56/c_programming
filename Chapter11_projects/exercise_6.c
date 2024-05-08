d6f54b731326cb363cc5e0dfe4dada950f3c007b
helene <helene.maja@hotmail.dk>
2024-05-08 22:52:27 +0200

    remove line

--- Chapter11_projects/exercise_6.c
+++ Chapter11_projects/exercise_6.c
@@ -22,13 +22,11 @@ void find_two_largest(int a[], int n, int *largest, int *second_largest)
     {
         if (a[i] > *largest)
         {
-            
             *second_largest = *largest;
             *largest = a[i];
         }
         else if (a[i] > *second_largest)
         {
-            
             *second_largest = a[i];
         }
         
