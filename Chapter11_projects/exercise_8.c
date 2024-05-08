2f37cd515cd8e514b60b90dfabb0354fd75dd1a0
helene <helene.maja@hotmail.dk>
2024-05-08 23:20:42 +0200

    add comments to exercise_8

--- Chapter11_projects/exercise_8.c
+++ Chapter11_projects/exercise_8.c
@@ -6,21 +6,19 @@ int main()
 {
     int a[5] = {9, 3, 0, 8, 2};
     int n = sizeof(a) / sizeof(a[0]);
-    printf("%d\n", find_largest(a, n));
+    printf("%d\n", *find_largest(a, n)); // uses * to dereference so i can get the value and NOT the address
 }
 
 int *find_largest(int a[], int n)
 {
-    int temp = 0;
-    int x;
-    for (int i = 0; i < n; i++)
+    int *largest = &a[0]; // initize a pointer that points to the address of the first element in a
+    for (int i = 1; i < n; i++)
     {
-        if (a[i] > temp)
+        if (a[i] > *largest) // checks if the value largest points to is larger than a[i], * dereferences it so instead of the address it now compares the value
         {
-            temp = a[i];
-            x = i;
+            largest = &a[i]; // pointer largest is updated to point to the current element in a, &a[i] captures the current address
         }
     }
 
-    return &a[x];
+    return largest; // returns the pointer largest
 }
