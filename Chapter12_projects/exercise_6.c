4ec764982ff2d3a91c4bc934600b8ea66f57c052
helene <helene.maja@hotmail.dk>
2024-05-09 19:30:49 +0200

    finish exercise 6

--- Chapter12_projects/exercise_6.c
+++ Chapter12_projects/exercise_6.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 // rewrite function to use pointer arithmetic instead of array subscipting
 
-int sum_array(const int a[], int n)
+int sum_array(const int *a, int n) // a* and a[] in functions both works as parameters
 {
-    int i, sum;
-    for (i = 0; i < n; i++)
+    int sum;
+    const int *p;
+    sum = 0;
+    for (p = a; p < a + n; p++)
     {
-        sum += a[i];
+        sum += *p;
     }
     return sum;
+}
+
+int main()
+{
+    int a[5] = {9, 3, 0, 8, 2};
+    int n = sizeof(a) / sizeof(a[0]); // gets the size of the array a
+
+    printf("sum: %d\n", sum_array(a, n));
 }
