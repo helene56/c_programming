71796552943490dde0908b697c204b95a76c02e7
helene <helene.maja@hotmail.dk>
2024-05-09 20:49:35 +0200

    finish exercise 9

--- Chapter12_projects/exercise_9.c
+++ Chapter12_projects/exercise_9.c
@@ -3,5 +3,27 @@
 
 double inner_product(const double *a, const double *b, int n)
 {
+    const double *p = a;
+    const double *q = b;
+    double sum = 0;
+    for (; p < a + n;)
+    {
+        sum += (*p * *q);
+        p++;
+        q++;
+    }
+    return sum;
     
+}
+
+int main()
+{
+    double a[5] = {1, 2, 2, 3, 4};
+    double b[5] = {1, 1, 2, 3, 4};
+    int n = sizeof(a) / sizeof(a[0]);
+    int sum;
+
+    sum = inner_product(a, b, n);
+    printf("%d\n", sum);
+
 }
