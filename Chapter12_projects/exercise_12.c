f304d5506ba22355d2e7be05b6ba56be5a544a52
helene <helene.maja@hotmail.dk>
2024-05-10 16:54:51 +0200

    finished exercise 12

--- Chapter12_projects/exercise_12.c
+++ Chapter12_projects/exercise_12.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 // write the following function
 
-void find_two_largest(const int *a, int n, int *largest, int *second_largest);
+void find_two_largest(const int *a, int n, int *largest, int *second_largest)
+{
+    *largest = *a; // initilize to the first element of the array
+    *second_largest = *a;
+    const int *p;
+    for (p = a; p < a + n; p++)
+    {
+        if (*p > *largest)
+        {
+            *second_largest = *largest;
+            *largest = *p;
+        } else if (*p > *second_largest)
+        {
+            *second_largest = *p;
+        }
+    }
+}
+
+int main()
+{
+    int a[5] = {5, 2, 3, 7, 2};
+    int n = sizeof(a) / sizeof(a[0]);
+    int largest;
+    int second_largest;
+
+    find_two_largest(a, n, &largest, &second_largest);
+
+    printf("largest: %d, second_largest: %d\n", largest, second_largest);
+}
