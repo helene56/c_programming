269a708756d3f8e55c5cfcfa6b4fe81db0a0f67f
helene <helene.maja@hotmail.dk>
2024-05-11 23:11:17 +0200

    add project1.c and project description

---
+++ Chapter12_projects/project1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// write a program that reads a message, then prints the reversal of the message
+// revise the program to use pointer instead of an integer to keep track of the current position in the array
+
+int main()
+{
+    printf("Enter a message: ");
+}
