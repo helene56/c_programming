9cb1ee970687b3e1c4734d50372269dc36725a5c
helene <helene.maja@hotmail.dk>
2024-05-12 12:59:25 +0200

    add prj2 an description

---
+++ Chapter12_projects/project2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// write a program that reads a mesage then checks wheter it's a palindrome
+// revise the prgram to use pointerd instead of integers to keep track of positions in the array
+
+
+int main()
+{
+
+}
