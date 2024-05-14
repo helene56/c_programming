571a059edd1cd204b1a978c0dff463342b5f2acd
helene <helene.maja@hotmail.dk>
2024-05-14 12:33:47 +0200

    add exercise 4 chapter 13, first iteration of exercise

---
+++ Chapter13_projects/exercise4.c
@@ -0,0 +1,32 @@
+// modify read_line function in each of the following ways
+// a) have it skip white space before beginning to store input characters
+// b) have it stop reading at the first white-space character
+#include <stdio.h>
+
+int read_line(char str[], int n)
+{
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')
+    {
+        if (i < n && ch != ' ')
+        {
+            str[i++] = ch;
+        }
+        
+        str[i] = '\0';
+        return i;
+    }
+}
+
+int main()
+{
+    char userInput;
+    char array[10];
+    int n = 10;
+
+    printf("Enter a sentence: ");
+
+    printf("%d", read_line(array, n));
+    printf("\n");
+}
