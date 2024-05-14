00a87e4c5e79902fdc0092d0841448f0b94d445e
helene <helene.maja@hotmail.dk>
2024-05-14 20:28:53 +0200

    add exercise 12

---
+++ Chapter13_projects/exercise12.c
@@ -0,0 +1,9 @@
+// write the following function:
+void get_extension(const char *file_name, char *extension);
+// file_name points to a string containing a file name. the function should store the extension on the file name in the string pointed to by extension.
+// if the file name does not have an extension the function should store an empty string (a single null character) in the string pointed to by extention.
+
+void get_extension(const char *file_name, char *extension)
+{
+    
+}
