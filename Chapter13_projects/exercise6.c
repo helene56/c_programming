b889a653d95e13bc1370f7585eef5f1c6d0bd57c
helene <helene.maja@hotmail.dk>
2024-05-14 15:21:18 +0200

    first iteration of exercise 6

--- Chapter13_projects/exercise6.c
+++ Chapter13_projects/exercise6.c
@@ -1,4 +1,26 @@
 // write a function named censor that modifies a string by replacing every occurrence of foo by xxx. 
 // make the function as short as possible
 #include <stdio.h>
+#include <string.h>
 
+void censor(char *str)
+{
+    char *p = str;
+    int len = strlen(str);
+
+    while ( p < str + len)
+    {
+        if (*p == 'f')
+        {
+            
+            if (*(p + 1) == 'o' && *(p + 2) == 'o')
+            {
+                *p = 'x';
+                *(p+1) = 'x';
+                *(p+2) = 'x';
+
+            }
+        }
+        p++;
+    }
+}
