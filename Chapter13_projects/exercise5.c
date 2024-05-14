f14e4adb8e858f1b885ccbe0534b9cd98c299ea4
helene <helene.maja@hotmail.dk>
2024-05-14 13:43:37 +0200

    add exercise 5 and beginning of function

---
+++ Chapter13_projects/exercise5.c
@@ -0,0 +1,7 @@
+// a) write a function named capitalize that capitalizes all letters in its argument. the argument will be a null-terminated string containing arbitrary characters not just letters.
+// b) rewrite the capitalize dunction, this time using pointer arithmetic to access the characters in the string.
+
+void capitalize(char *str)
+{
+    
+}
