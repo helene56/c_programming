7bbfff25272367a6eb7fca1b4428c00ff1645810
helene <helene.maja@hotmail.dk>
2024-05-15 21:48:35 +0200

    main function to read numbers enter by user set up

--- Chapter13_projects/project1.c
+++ Chapter13_projects/project1.c
@@ -1,4 +1,32 @@
 // write a program fhat finds the "smallest" and "largest" in a series of words. 
 // after the user enters the words, the program will determine which words would come first and last if the words were listed in dictionary order.
 // the program must stop axxepting input ehrn the user enters a four-letter word.
-// assume that no word is more than 20 letters long. 
+// assume that no word is more than 20 letters long.
+
+#include <stdio.h>
+#include <string.h>
+
+void compare_strings(char *largest_string, char *smallest_string);
+
+int main()
+{
+    char smallest[20];
+    char largest[20];
+    char userInput[20];
+    int user_len = strlen(userInput);
+    
+    while (user_len != 4)
+    {
+        printf("Enter word: ");
+        scanf("%s", userInput);
+        user_len = strlen(userInput);
+        printf("%d\n", user_len);
+    }
+
+}
+
+void compare_strings(char *largest_string, char *smallest_string)
+{
+
+}
+
