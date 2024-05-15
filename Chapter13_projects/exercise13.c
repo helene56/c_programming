ff101a799035aadc341352d319ca87b33b6bba92
helene <helene.maja@hotmail.dk>
2024-05-15 14:01:35 +0200

    finish first iteration of function build_index_url

--- Chapter13_projects/exercise13.c
+++ Chapter13_projects/exercise13.c
@@ -2,10 +2,17 @@
 void build_index_url(const char *domain, char *index_url);
 // domain points to a string containing an Internet domain (knking.com). the function should add http://www. to the beginning of this string and /index.html to the end,
 // storing the result in the string pointed to by index_url
+#include <stdio.h>
+#include <string.h>
 
 void build_index_url(const char *domain, char *index_url)
 {
     // steps: copy http://www into index_url
     // use strcat to append the string from domain after index_url
     // use strcat to append /index.html to index_url
+    const char *p = domain;
+
+    strcpy(index_url, "http://www.");
+    strcat(index_url, p);
+    strcat(index_url, "/index.html");
 }
