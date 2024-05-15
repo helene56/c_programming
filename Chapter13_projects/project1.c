02bc64d107629aa8c426a1ea10c28ae1cfc2b336
helene <helene.maja@hotmail.dk>
2024-05-15 22:14:36 +0200

    update to main()

--- Chapter13_projects/project1.c
+++ Chapter13_projects/project1.c
@@ -6,27 +6,39 @@
 #include <stdio.h>
 #include <string.h>
 
-void compare_strings(char *largest_string, char *smallest_string);
+void compare_strings(char *largest_string, char *smallest_string, const char *user_string);
 
 int main()
 {
-    char smallest[20];
-    char largest[20];
+    char smallest[20] = "";
+    char largest[20] = "";
     char userInput[20];
     int user_len = strlen(userInput);
+
+    printf("Enter word: ");
+    scanf("%s", userInput);
+    strcpy(smallest, userInput); // initiliaze first word to the smallest
     
+
     while (user_len != 4)
     {
         printf("Enter word: ");
         scanf("%s", userInput);
         user_len = strlen(userInput);
-        printf("%d\n", user_len);
+        // printf("%d\n", user_len);
+        compare_strings(largest, smallest, userInput);
+        printf("%s\n", smallest);
+
     }
 
 }
 
-void compare_strings(char *largest_string, char *smallest_string)
+void compare_strings(char *largest_string, char *smallest_string, const char *user_string)
 {
-
+    if (strcmp(user_string, smallest_string) < 0)
+    {
+        strcpy(smallest_string, user_string);
+        // printf("%s", smallest_string);
+    }
 }
 
