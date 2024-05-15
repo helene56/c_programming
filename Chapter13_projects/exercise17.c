917117af10d1db88f694d2999945060355786e58
helene <helene.maja@hotmail.dk>
2024-05-15 14:30:07 +0200

    first iteration of function test_extension not taking account for capitalized letters yet

--- Chapter13_projects/exercise17.c
+++ Chapter13_projects/exercise17.c
@@ -3,6 +3,27 @@
 #include <string.h>
 // write the following function:
 bool test_extension(const char *file_name, const char *extension);
-// file_name points to astring containing a file name. The function should return true if the files's extension matches the string pointed to by extension.
+// file_name points to a string containing a file name. The function should return true if the files's extension matches the string pointed to by extension.
 // incorporate the "search for the end of a string" idiom into your function.
 
+
+bool test_extension(const char *file_name, const char *extension)
+{
+    const char *p = file_name;
+
+    while (*p)
+    {
+        if (*p++ == '.');
+        if (strcmp(p, extension))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+        p++;
+        
+    }
+}
