4b0e386375710f652017fec98532da71910a16c7
helene <helene.maja@hotmail.dk>
2024-05-15 14:20:38 +0200

    add exercise 17

---
+++ Chapter13_projects/exercise17.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+// write the following function:
+bool test_extension(const char *file_name, const char *extension);
+// file_name points to astring containing a file name. The function should return true if the files's extension matches the string pointed to by extension.
+// incorporate the "search for the end of a string" idiom into your function.
+
