5bc4ed3f7af9ed2038025a3107edc177792f10dd
helene <helene.maja@hotmail.dk>
2024-05-15 13:52:58 +0200

    add exercise 13 and description

---
+++ Chapter13_projects/exercise13.c
@@ -0,0 +1,5 @@
+// write the following function:
+void build_index_url(const char *domain, char *index_url);
+// domain points to a string containing an Internet domain (knking.com). the function should add http://www. to the beginning of this string and /index.html to the end,
+// storing the result in the string pointed to by index_url
+
