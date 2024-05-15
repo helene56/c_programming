c579e732e0b2a40daf873f930017b8d3b4565986
helene <helene.maja@hotmail.dk>
2024-05-15 21:23:41 +0200

    add project1

---
+++ Chapter13_projects/project1.c
@@ -0,0 +1,4 @@
+// write a program fhat finds the "smallest" and "largest" in a series of words. 
+// after the user enters the words, the program will determine which words would come first and last if the words were listed in dictionary order.
+// the program must stop axxepting input ehrn the user enters a four-letter word.
+// assume that no word is more than 20 letters long. 
