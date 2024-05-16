c9ce03a1c32ee5382f92f79cef695e6fb40642f2
helene <helene.maja@hotmail.dk>
2024-05-16 15:44:01 +0200

    adjustment of main function and add library string.h

--- Chapter13_projects/project18.c
+++ Chapter13_projects/project18.c
@@ -2,19 +2,36 @@
 // and then displays it in the form month dd, yyyy where month is the name of the month
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     printf("Enter a date (mm/dd/yyyy): ");
-    char user_input[20];
+    char user_input[10 + 1];
     char *months[] = {"January", "February", "March", "April", 
                       "May", "Juni", "July", "August", 
                       "September", "October", "November", "December"};
     scanf("%s", user_input);
     
     char *p = user_input;
-    while (*user_input)
+    char user_month[3];
+
+   
+    for (int i = 0; i < 2; i++)
     {
-        
+        user_month[i] = *p++;
+        // char *user_month = month[] 
     }
+    int month = atoi(user_month);
+
+    char day_year[9];
+    while (*p++ != '/');
+    strcpy(p, day_year);
+    
+
+
+    printf("You entered the date %s %s", months[month], day_year);
+
+
+    
 }
