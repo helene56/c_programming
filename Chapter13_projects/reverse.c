6d4745a6bcba81bd0faaf80c8e371b944e1a77b5
helene <helene.maja@hotmail.dk>
2024-05-16 10:57:07 +0200

    delete unessecary comments

--- Chapter13_projects/reverse.c
+++ Chapter13_projects/reverse.c
@@ -8,12 +8,6 @@
 // argc(argument count) is the number of command-line arguments, argv(argument vector) is an array of pointers to the command-line arguments which are stored in string form
 int main(int argc, char *argv[])
 {
-    // int i;
-    // for (i = 1; i < argc; i++)
-    // {
-    //     printf("%s\n", argv[i]);
-    // }
-    // printf("%d", argc);
     printf("%s ", argv[3]);
     printf("%s ", argv[2]);
     printf("%s\n", argv[1]);
