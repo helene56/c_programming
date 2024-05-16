b3bcd5dd94d8844bf5e6c88c86223727581ae592
helene <helene.maja@hotmail.dk>
2024-05-16 15:15:37 +0200

    add general main function structure to project 18

--- Chapter13_projects/project18.c
+++ Chapter13_projects/project18.c
@@ -1,3 +1,20 @@
 // write a program that accepts a date from the user in the form mm/dd/yyyy
-// and then dispæays it in the form month dd, yyyy where month is the name of the month
+// and then displays it in the form month dd, yyyy where month is the name of the month
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    printf("Enter a date (mm/dd/yyyy): ");
+    char user_input[20];
+    char *months[] = {"January", "February", "March", "April", 
+                      "May", "Juni", "July", "August", 
+                      "September", "October", "November", "December"};
+    scanf("%s", user_input);
+    
+    char *p = user_input;
+    while (*user_input)
+    {
+        
+    }
+}
