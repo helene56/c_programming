342c9bd9a87aa7c2f4300230283b9fd7fb4a94c7
helene <helene.maja@hotmail.dk>
2024-05-16 14:27:54 +0200

    add project 5

---
+++ Chapter13_projects/project5.c
@@ -0,0 +1,5 @@
+// write a program named sum.c that adds up its command-line arguments, which are assumed to be integers. Running the program by typing:
+// sum 8 24 62
+// should produce the following output:
+// total: 94
+
