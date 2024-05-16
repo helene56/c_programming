39bf3e08f165f80a1f93947e005092fadb8c3ab7
helene <helene.maja@hotmail.dk>
2024-05-16 10:34:33 +0200

    add project 4

---
+++ Chapter13_projects/project4.c
@@ -0,0 +1,9 @@
+// write a program named reverse.c that echoes its command-line arguments in reverse order. running the program by typing:
+// reverse void and null
+// should produce the following output:
+// null and void
+
+#include <stdio.h>
+
+// argc(argument count) is the number of command-line arguments, argv(argument vector) is an array of pointers to the command-line arguments which are stored in string form
+int main(int argc, char *argv[]) 
