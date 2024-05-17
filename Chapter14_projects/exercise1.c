f629ffca877cf56cb1ece2006f9175dc7fa379ce
helene <helene.maja@hotmail.dk>
2024-05-17 23:07:38 +0200

    finish exercise 1

--- Chapter14_projects/exercise1.c
+++ Chapter14_projects/exercise1.c
@@ -2,10 +2,17 @@
 // a. the cube of x
 // b. the remainder when n is divided by 4
 // c. 1 if the product of x and y is less than 100, 0 otherwise
-
-#define CUBE(x) (x*x)
+#include <stdio.h>
+#define CUBE(x) ((x)*(x)*(x)) // task a.
+#define REMAINDER(n) ((n) % 4) // task b.
+#define PRODUCT(x,y) (((x)*(y))< 100 ? 1 : 0)
 
 int main()
 {
-    printf("%d", CUBE(2));
+    int x = 2;
+    int y = 400;
+    printf("the cubed value of %d is %d\n", x, CUBE(x));
+    printf("the remainder of %d is %d when divided by 4\n", x, REMAINDER(x));
+    printf("%d\n", PRODUCT(x,y));
+
 }
