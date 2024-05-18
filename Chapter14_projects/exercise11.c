93ccd6e536e1bc89c1dfd0903817cf508c2fe1f5
helene <helene.maja@hotmail.dk>
2024-05-18 15:48:25 +0200

    finish exercise 11

--- Chapter14_projects/exercise11.c
+++ Chapter14_projects/exercise11.c
@@ -1,3 +1,11 @@
 // write a macro named ERROR that generates the call of fprintf whown above when given a format string and the items to be displayed
+#include <stdio.h>
 
-#define ERROR(string, ...) fprintf(stderr, string, __VA_ARGS__)
+#define ERROR(string, ...) (fprintf(stderr, (string), __VA_ARGS__))
+
+int main()
+{
+    int index = 1;
+   
+    ERROR("Range error: index = %d\n", index);
+}
