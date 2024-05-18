e300c9de40bc46d132ffe4aed0c02b6c60cc9be9
helene <helene.maja@hotmail.dk>
2024-05-18 13:17:39 +0200

    add exercise 9

---
+++ Chapter14_projects/exercise9.c
@@ -0,0 +1,5 @@
+// write the following parametierized macros
+// (a) CHECK(x,y,n) - has the value 1 if both x and y fall between 0 and n-1, inclusive.
+// (b) MEDIAN(x,y,z) - finds the median of x,y, and z.
+// (c) POLYNOMIAL(x) - computes the polynomial 3x^5+2x^4-5x^3-x^2+7x-6.
+
