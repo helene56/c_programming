2de28b64d38becf1aa1a9b9ad963ae8de492fb70
helene <helene.maja@hotmail.dk>
2024-05-18 12:58:41 +0200

    finish task a of exercise 6

--- Chapter14_projects/exercise6.c
+++ Chapter14_projects/exercise6.c
@@ -1,3 +1,12 @@
 // a. write a macro DISP(f,x) that expands into a call of printf that displays the value of the funcion f when called with argument x.
 // b. write a macro DISP2(f,x,y) that's similar to DISP but works for functions with two arguments.
 
+#include <stdio.h>
+#include <math.h>
+
+#define DISP(f,x) printf(#f "(%g) = %g\n", x, f(x));
+
+int main()
+{
+    DISP(sqrt, 3.0);
+}
