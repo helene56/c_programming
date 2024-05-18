b057f3faf72a49258ea604b7360cb9071df29ac1
helene <helene.maja@hotmail.dk>
2024-05-18 12:50:39 +0200

    add exercise 6

---
+++ Chapter14_projects/exercise6.c
@@ -0,0 +1,3 @@
+// a. write a macro DISP(f,x) that expands into a call of printf that displays the value of the funcion f when called with argument x.
+// b. write a macro DISP2(f,x,y) that's similar to DISP but works for functions with two arguments.
+
