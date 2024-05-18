17d66e086d43dad71670701853c32718b656c27a
helene <helene.maja@hotmail.dk>
2024-05-18 13:26:04 +0200

    finish task a of exercise 9

--- Chapter14_projects/exercise9.c
+++ Chapter14_projects/exercise9.c
@@ -3,3 +3,16 @@
 // (b) MEDIAN(x,y,z) - finds the median of x,y, and z.
 // (c) POLYNOMIAL(x) - computes the polynomial 3x^5+2x^4-5x^3-x^2+7x-6.
 
+#include <stdio.h>
+#define CHECK(x, y, n) (((x) >= 0 && (x) <= ((n)-1))&&((y) >= 0 && (y) <= ((n)-1)) ? 1 : 0)
+
+int main()
+{
+    int x = 11;
+    int y = 2;
+    int n = 10;
+
+    
+    printf("%d\n", CHECK(x, y, n));
+
+}
