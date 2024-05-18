3e7ae7933464c59b7626da37abfaa965facf291e
helene <helene.maja@hotmail.dk>
2024-05-18 13:03:54 +0200

    finish task b. of exercise 6

--- Chapter14_projects/exercise6.c
+++ Chapter14_projects/exercise6.c
@@ -5,8 +5,10 @@
 #include <math.h>
 
 #define DISP(f,x) printf(#f "(%g) = %g\n", x, f(x));
+#define DISP2(f,x,y) printf(#f "(%g,%g) = %g\n", x, y, f(x,y));
 
 int main()
 {
     DISP(sqrt, 3.0);
+    DISP2(pow, 3.0, 2.0);
 }
