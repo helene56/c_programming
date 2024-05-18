4f4ecfb8bced7825de616f8a3989e742a0a22fed
helene <helene.maja@hotmail.dk>
2024-05-18 16:54:38 +0200

    finish exercise 15

--- Chapter14_projects/exercise15.c
+++ Chapter14_projects/exercise15.c
@@ -1,10 +1,16 @@
 // suppose that program needs to display messages in either English, French or Spanish.
 // using conditional compilation, write a program fragment that displays one of the following three messages depending on whether or nortthe specified macro is defined.
+#include <stdio.h>
+#define ENGLISH
 
-#if defined ENGLISH
-    printf("Insert Disk 1");
-#elif defined FRENCH
-    printf("In");
-#elif defined SPANISH
-#endif
 
+int main()
+{
+    #if defined ENGLISH
+        printf("Insert Disk 1\n");
+    #elif defined FRENCH
+        printf("Inserez Le Disque 1\n");
+    #elif defined SPANISH
+        printf("Inserte El Disco 1\n");
+    #endif
+}
