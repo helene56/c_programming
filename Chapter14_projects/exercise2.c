4359f663407bf2e7b444dc21f6d3c13990c241b5
helene <helene.maja@hotmail.dk>
2024-05-18 12:46:46 +0200

    finish exercise 2

--- Chapter14_projects/exercise2.c
+++ Chapter14_projects/exercise2.c
@@ -1,1 +1,11 @@
 // write a macro NELEMS(a) that computes the number of elements in a one-dimensional array a.
+
+#include <stdio.h>
+
+#define NELEMS(a) ((sizeof(a)) / (sizeof(a[0])))
+
+int main()
+{
+    int a[] = {1, 2, 2, 3};
+    printf("size of aray: %d\n", NELEMS(a));
+}
