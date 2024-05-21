7467db82d62b5d8e3dd6a2c6b04b95e8a0f346b2
helene <helene.maja@hotmail.dk>
2024-05-21 23:41:32 +0200

    first iteration of exercise 5 finish

--- Chapter16_projects/exercise5/exercise5.c
+++ Chapter16_projects/exercise5/exercise5.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdio.h>
 struct date {
     int month;
     int day;
@@ -12,8 +13,8 @@ int day_of_year(struct date d)
     int total_days;
     int temp_february;
     bool leapyear = false;
-    leapyear = ((d.year % 400 == 0) ? (d.year % 100 == 0 ? (d.year % 4 == 0) : 0) : 0);
-    printf("leapyear is: %d", leapyear);
+
+    leapyear = (d.year % 400 == 0) ? 1 : (d.year % 100 == 0) ? 0 : (d.year % 4 == 0) ? 1 : 0;
     //02 month days = 29 total days = 366
     // else 02 month days = 28, total days = 365
     if (leapyear)
@@ -30,12 +31,21 @@ int day_of_year(struct date d)
     int days_in_month = 0;
 
     int month_days[] = {31, temp_february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    for (int i = 0; i < d.month; i++)
+    for (int i = 0; i < d.month - 1; i++)
     {
         days_in_month += month_days[i];
     }
 
     int current_day = days_in_month + d.day;
 
+    return current_day;
+}
 
+
+int main()
+{
+    struct date date1 = {3, 5, 2024};
+
+    printf("the date is %d\n", day_of_year(date1));
+    
 }
