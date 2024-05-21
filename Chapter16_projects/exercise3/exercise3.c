d9cdec5162d765fd57faa8a725b6c311f16ef80a
helene <helene.maja@hotmail.dk>
2024-05-21 15:34:37 +0200

    add task d to exercise 3

--- Chapter16_projects/exercise3/exercise3.c
+++ Chapter16_projects/exercise3/exercise3.c
@@ -15,3 +15,13 @@ struct complex make_complex(double real, double imaginary)
     c.imaginary = imaginary;
     return c;
 }
+// notes: could have used a compound literal as C99+ allows for this
+struct complex make_complex(double real, double imaginary) {
+    return (struct complex) {real, imaginary};
+}
+
+// (d)
+struct complex add_complex(struct complex c1, struct complex c2)
+{
+    return (struct complex) {.real = c1.real + c2.real, .imaginary = c1.imaginary + c2.imaginary};
+}
