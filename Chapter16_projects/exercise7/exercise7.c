ba6d0b2af2e39e67edbc2f480ce69b1ea042d671
Helene Graff-Nielsen <helene.maja@hotmail.dk>
2024-05-26 12:40:16 +0200

    finish task (b)

--- Chapter16_projects/exercise7/exercise7.c
+++ Chapter16_projects/exercise7/exercise7.c
@@ -21,13 +21,39 @@ struct fraction reduce(struct fraction f)
     
     f.numerator = f.numerator/a;
     f.denominator = f.denominator/a;
-    printf("a: %d, b: %d\n", a, b);
-    printf("numerator: %d, denominator: %d\n", f.numerator, f.denominator);
+
+    return f;
     
 }
+
+// (b)
+struct fraction add(struct fraction f1, struct fraction f2)
+{
+    f1.numerator = f1.numerator * f2.denominator;
+    f2.numerator = f2.numerator * f1.denominator;
+
+    int total_numerator = f1.numerator + f2.numerator;
+    int total_denominator = f1.denominator * f2.denominator;
+
+    struct fraction f_total = {total_numerator, total_denominator};
+    f_total = reduce(f_total);
+    
+    return f_total;
+}
+
+
+
 
 int main()
 {
     struct fraction f = {48, 18};
-    reduce(f);
+    struct fraction f1 = {2, 6};
+    struct fraction f2 = {4, 9};
+    struct fraction f_new = reduce(f);
+    printf("newf numerator: %d, newf denominator: %d\n", f_new.numerator, f_new.denominator);
+
+    struct fraction f_result = add(f1, f2);
+    printf("add output: numerator: %d, denominator: %d\n", f_result.numerator, f_result.denominator);
+    
+
 }
