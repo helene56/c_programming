4f48e975ce512becdd04315e353194d9c7f5a4d5
Helene Graff-Nielsen <helene.maja@hotmail.dk>
2024-05-26 17:25:55 +0200

    finish excercise 7, could have made functions a bit more compact

--- Chapter16_projects/exercise7/exercise7.c
+++ Chapter16_projects/exercise7/exercise7.c
@@ -41,8 +41,44 @@ struct fraction add(struct fraction f1, struct fraction f2)
     return f_total;
 }
 
+// (c)
+struct fraction subtract(struct fraction f1, struct fraction f2)
+{
+    f1.numerator = f1.numerator * f2.denominator;
+    f2.numerator = f2.numerator * f1.denominator;
 
+    int total_numerator = f1.numerator - f2.numerator;
+    int total_denominator = f1.denominator * f2.denominator;
 
+    struct fraction f_total = {total_numerator, total_denominator};
+    f_total = reduce(f_total);
+    
+    return f_total;
+}
+
+// (d)
+struct fraction multiply(struct fraction f1, struct fraction f2)
+{
+    int numerator = f1.numerator * f2.numerator;
+    int denominator = f1.denominator * f2.denominator;
+
+    return reduce((struct fraction) {.numerator = numerator, .denominator = denominator});
+}
+
+// (e)
+struct fraction divide(struct fraction f1, struct fraction f2)
+{
+    int a = f2.numerator;
+    int b = f2.denominator;
+
+    f2.denominator = a;
+    f2.numerator = b;
+
+    struct fraction f = multiply(f1, f2);
+    f = reduce(f);
+
+    return f;
+}
 
 int main()
 {
@@ -55,5 +91,9 @@ int main()
     struct fraction f_result = add(f1, f2);
     printf("add output: numerator: %d, denominator: %d\n", f_result.numerator, f_result.denominator);
     
+    struct fraction multiply_result = multiply(f1, f2);
+    printf("result of multiply: numerator: %d, denominator: %d\n", multiply_result.numerator, multiply_result.denominator);
 
+    struct fraction divide_result = divide(f1, f2);
+    printf("result of divide: numerator: %d, denominator: %d\n", divide_result.numerator, divide_result.denominator);
 }
