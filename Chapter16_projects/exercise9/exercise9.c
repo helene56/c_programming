fbeb83da212c3c72d0e5bbe7ed6011aa59ef6b2c
helene <helene.maja@hotmail.dk>
2024-05-27 16:44:39 +0200

    update to readme.md and finish task (d)
    q

--- Chapter16_projects/exercise9/exercise9.c
+++ Chapter16_projects/exercise9/exercise9.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 struct color {
     int red;
@@ -36,3 +38,61 @@ struct color make_color(int red, int green, int blue)
 
     return (struct color) {red, green, blue};
 }
+
+// (b)
+int getRed(struct color c)
+{
+    return c.red;
+}
+
+// (c) could have returned the equation directly as if it was true it would evalute to true
+bool equal_color(struct color color1, struct color color2)
+{
+    if (color1.red == color2.red && color1.blue == color2.blue && color1.green == color2.green)
+    {
+        return true;
+    }
+    return false;
+}
+
+// (d)
+struct color brighter(struct color c)
+{
+    if (c.blue == 0 && c.green == 0 && c.red == 0)
+    {
+        return (struct color) {.red = 3, .green = 3, .blue = 3};
+    }
+    if (c.blue > 0 && c.blue < 3)
+    {
+        c.blue = 3;
+    }
+    if (c.green > 0 && c.green < 3)
+    {
+        c.green = 3;
+    }
+    if (c.red > 0 && c.red < 3)
+    {
+        c.red = 3;
+    }
+
+    c.blue = (int) c.blue/0.7;
+    c.green = (int) c.green/0.7;
+    c.red = (int) c.red/0.7;
+
+    if (c.red > 255)
+    {
+        c.red = 255;
+    }
+    if (c.blue > 255)
+    {
+        c.blue = 255;
+    }
+    if (c.green > 255)
+    {
+        c.green = 255;
+    }
+
+    return c;
+}
+
+// (e)
